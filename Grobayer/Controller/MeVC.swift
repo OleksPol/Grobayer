07402b6799dc0c9c043108d4588ab4ad876c5cc5
Alexandr <Sashob84@gmail.com>
2018-12-28 15:59:32 +0200

    setup UserVC

---
+++ Grobayer/Controller/MeVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeVC.swift
+//  Grobayer
+//
+//  Created by Alexandr on 12/28/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+}
